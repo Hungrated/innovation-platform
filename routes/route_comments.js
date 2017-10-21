1cb0e4afe094e4f20dcf926d7b335d93a4420a07
Zihang Zhang <zhang295415658@qq.com>
2017-10-21 14:42:57 +0800

    comment interface established

--- routes/route_comments.js
+++ routes/route_comments.js
@@ -7,5 +7,26 @@ const router = express.Router();
 const Comment = require('../models/comments');
 const statusLib = require('../config/status');
 
+router.post('/', function (req, res) {
+  const {
+    student_id,
+    article_id,
+    content
+  } = req.body;
+  Comment.create({
+    content,
+    student_id,
+    article_id
+  })
+    .then(function () {
+      res.json(statusLib.COMMENT_SUCCEEDED);
+      console.log('comment succeeded');
+    })
+    .catch(function (e) {
+      console.error(e);
+      res.json(statusLib.COMMENT_FAILED);
+      console.log('publish failed');
+    });
+});
 
 module.exports = router;
