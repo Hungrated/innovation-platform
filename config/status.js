4759244f0625990081c7cfd6f03bce01b7b882d0
Zihang Zhang <zhang295415658@qq.com>
2017-10-23 14:55:30 +0800

    statusLib modified

--- config/status.js
+++ config/status.js
@@ -4,127 +4,127 @@
 
 module.exports = {
   REG_SUCCEEDED: {
-    status: 100,
+    status: 1000,
     msg: '注册成功'
   },
 
   REG_FAILED: {
-    status: 101,
+    status: 1001,
     msg: '注册失败'
   },
 
   LOGIN_SUCCEEDED: {
-    status: 200,
+    status: 1100,
     msg: '登录成功'
   },
 
   LOGIN_FAILED: {
-    status: 201,
+    status: 1101,
     msg: '登录失败'
   },
 
   ALREADY_LOGGED_IN: {
-    status: 202,
+    status: 1102,
     msg: '已是登录状态'
   },
 
   NOT_YET_LOGGED_IN: {
-    status: 203,
+    status: 1103,
     msg: '尚未登录状态'
   },
 
   LOGGED_OUT: {
-    status: 300,
+    status: 1200,
     msg: '已退出登录'
   },
 
   LOG_OUT_ERROR: {
-    status: 301,
+    status: 1201,
     msg: '退出异常'
   },
 
   PROFILE_MOD_SUCCEEDED: {
-    status: 400,
+    status: 2000,
     msg: '档案更新成功'
   },
 
   PROFILE_MOD_FAILED: {
-    status: 401,
+    status: 2001,
     msg: '档案更新失败'
   },
 
   PROFILE_FETCH_FAILED: {
-    status: 402,
+    status: 2101,
     msg: '档案获取失败'
   },
 
   BLOG_PUB_SUCCEEDED: {
-    status: 500,
+    status: 3000,
     msg: '博文发布成功'
   },
 
   BLOG_PUB_FAILED: {
-    status: 501,
+    status: 3001,
     msg: '博文发布失败'
   },
 
   BLOG_LIST_FETCH_FAILED: {
-    status: 502,
+    status: 3101,
     msg: '获取博文列表失败'
   },
 
   BLOG_DETAILS_FETCH_FAILED: {
-    status: 503,
+    status: 3102,
     msg: '获取博文详情失败'
   },
 
   COMMENT_SUCCEEDED: {
-    status: 600,
+    status: 3200,
     msg: '评论成功'
   },
 
   COMMENT_FAILED: {
-    status: 601,
+    status: 3201,
     msg: '评论失败'
   },
 
   FILE_UPLOAD_SUCCEEDED: {
-    status: 700,
+    status: 4000,
     msg: '资源文件上传成功'
   },
 
   FILE_UPLOAD_FAILED: {
-    status: 701,
+    status: 4001,
     msg: '资源文件上传失败'
   },
 
   FILE_RENAME_FAILED: {
-    status: 702,
+    status: 4101,
     msg: '资源文件重命名失败'
   },
 
   FILE_INFO_FETCH_ERROR: {
-    status: 703,
+    status: 4102,
     msg: '资源文件信息错误'
   },
 
   PLAN_SUBMIT_SUCCEEDED: {
-    status: 801,
+    status: 5000,
     msg: '计划提交成功'
   },
 
   PLAN_SUBMIT_FAILED: {
-    status: 802,
+    status: 5001,
     msg: '计划提交失败'
   },
 
   PLAN_RATE_SUCCEEDED: {
-    status: 901,
+    status: 5100,
     msg: '计划评价成功'
   },
 
   PLAN_RATE_FAILED: {
-    status: 902,
+    status: 5101,
     msg: '计划评价失败'
   },
 
