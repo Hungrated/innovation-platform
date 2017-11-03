77df7d609871403c494d1db0a62e4570413c3016
zhujiayan <yourzhu@126.com>
2017-11-03 10:31:25 +0800

    修改活动、资源页面和js

--- public/scripts/commentDetail.js
+++ public/scripts/commentDetail.js
@@ -2,3 +2,36 @@
  * Created by wuli等等 on 2017/10/19.
  */
 $("#commentPage").addClass("active");
+
+$(".inputSubmit").on("click",function () {
+    $.ajax({
+        type: "POST",                   //类型，POST或者GET
+        url: "http://localhost:3000/api/comments/submit",        //后台url
+        data: {                          //数据
+            student_id: localStorage.getItem(username),
+            article_id: password
+        },
+        dataType: 'json',              //数据返回类型，可以是xml、json等
+        success: function (data) {      //成功，回调函数
+            console.log(data);
+            if (data.status == 1100){
+                localStorage.username = data.username;
+                location.href = ("../index.html");
+
+            } else {
+                var dialog = art.dialog({
+                    title: '提示',
+                    content: data.msg,
+                    lock:true,
+                    ok:true,
+                    follow: document.getElementById('logoNav')
+                });
+                reset();
+            }
+        },
+        error: function (err) {          //失败，回调函数
+            alert('未知错误，登录失败！');
+            console.log(err);
+        }
+    });
+});
--- public/scripts/resource.js
+++ public/scripts/resource.js
@@ -2,3 +2,55 @@
  * Created by wuli等等 on 2017/10/19.
  */
 $("#resource").addClass("active");
+$("#uploadfile").on("click",function () {
+    var fp = $("#file");
+    var lg = fp[0].files.length; // get length
+    var items = fp[0].files;
+
+    if (lg > 0) {
+        for (var i = 0; i < lg; i++) {
+            var fileName = items[i].name; // get file name
+            var fileSize = items[i].size; // get file size
+            var fileType = items[i].type; // get file type
+        }
+
+        console.log(fileName);
+        console.log(fileSize);
+        console.log(fileType);
+    }
+
+    $.ajaxFileUpload({
+        //处理文件上传操作的服务器端地址
+        url: 'http://localhost:3000/api/file/upload',
+        enctype: "multipart/form-data",
+        secureuri: false,                       //是否启用安全提交,默认为false
+        fileElementId: 'file',                        //文件选择框的id属性
+        dataType: "json",                       //服务器返回的格式,可以是json或xml等
+        //contentType:"application/json",
+        data: {
+            filename: fileName,
+            size:fileSize
+        },
+        success: function (data) {
+            debugger;
+            if (data.status == 4000) {
+                console.log(data.msg);
+                //window.location.reload();
+            }
+            else{
+                debugger;
+                console.log(data.msg)
+            }
+        },
+        error: function (msg) {
+            console.log(msg.responseText);
+        }
+    });
+});
+
+
+$("#selectF").on("click",function () {;
+    $("#uploadfile").css("display","block");
+
+    // this.style.display = "none";
+});
