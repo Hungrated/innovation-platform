b02f70826e27d1ae9b0eadb108d68c153b3b0557
zhujiayan <yourzhu@126.com>
2017-11-11 18:29:56 +0800

    文件修改

--- public/scripts/resource.js
+++ public/scripts/resource.js
@@ -3,8 +3,8 @@
  */
 // 全局变量 待优化
 var fileName = new Array();
-var fileSize = new Array();
-var i=0;
+
+var i;
 $(function () {
      $.ajax({
         type: "POST",                   //类型，POST或者GET
@@ -14,35 +14,44 @@ $(function () {
          },
          dataType: 'json',              //数据返回类型，可以是xml、json等
          success: function (data) {      //成功，回调函数
-            console.log(data);
-             // 待定
+             var taskListData = {
+                 taskLists:data
+             };
+             //console.log(postListData);
+             var post = template('taskList', taskListData);
+             $("#taskListContainer").html(post);
          },
        error: function (err) {          //失败，回调函数
              console.log(err);
          }
      });
-    $('body').on('change',$('#ImportPicInput'),function(){
-        $( "#importPicName").val($( "#ImportPicInput").val());
-    });
+     i=1;
 });
 $("#resource").addClass("active");
 
 // 上传多个文件和学号、描述
 $("#uploadfile").on("click",function () {
-    debugger;
+    var des ='';
+    var Texts = $(".desinput");
+    for(var j=0;j<Texts.length-1;j++)
+    {
+        des = des +Texts[j].value+ ',';
+    }
+    des = des +Texts[j].value;
+    console.log(des);
+    console.log(fileName);
     $.ajaxFileUpload({
         //处理文件上传操作的服务器端地址
         url: 'http://localhost:3000/api/file/upload',
-        enctype: "multipart/form-data",
+        enctype: "multipart/form-datas",
         secureuri: false,                       //是否启用安全提交,默认为false
-        fileElementId: ['file1','file2'],                        //文件选择框的id属性
+        fileElementId: fileName,                        //文件选择框的id属性
         dataType: "json",                       //服务器返回的格式,可以是json或xml等
         data: {
             school_id:localStorage.school_id,
-            descriptions:'a,a'
+            descriptions:des
         },
         success: function (data) {
-            debugger;
             // window.location.reload();
             if (data.status == 4000) {
                 var dialog = art.dialog({
@@ -54,12 +63,10 @@ $("#uploadfile").on("click",function () {
                 });
             }
             else{
-                debugger;
                 alert(data.msg)
             }
         },
         error: function (msg) {
-            debugger;
             console.log(msg.responseText);
         }
     });
@@ -74,7 +81,15 @@ $("#uploadfile").on("click",function () {
 //
 //     // this.style.display = "none";
 // });
-
+function addNew(obj) {
+    $(obj).parent("a").hide();
+    fileName.push('file'+i)
+    var dess = '<input type="text" class="form-control input-xl desinput"  placeholder="">';
+    $("#textArr").append(dess);
+    i++;
+    var str = '<a  class="file pull-right">选择文件<input onchange="addNew(this)"  type="file" name="files" id="file'+i+'" ></a>';
+    $("#uploadArr").append(str);
+}
 
 
 $("#uploadDiv").on("click",function(){
