e87eaf21750c90e275c6f52f2c898410a633ae29
Zerocan <1285711756@qq.com>
2017-11-12 16:16:20 +0800

    活动 评论页面 细节修改

--- public/scripts/commentDetail.js
+++ public/scripts/commentDetail.js
@@ -35,3 +35,18 @@ $(".inputSubmit").on("click",function () {
         }
     });
 });
+
+//原插件代码
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-49937515-1', 'auto');
+ga('send', 'pageview');
+
+document.body.className = document.body.className.replace(/(^|\s)is-noJs(\s|$)/, "$1is-js$2");
+
+/* <![CDATA[ */
+var obvInit = {"api":"https:\/\/fatesinger.com\/__api\/","nonce":"4a8d3d57b4","user":"0","permalink":"https:\/\/fatesinger.com\/78012","id":"78012","single":"1","preview":"","webp":"image\/webp,*\/*;q=0.8"};
+/* ]]> */
---
+++ public/scripts/comments.js
@@ -0,0 +1,4 @@
+/**
+ * Created by wuli等等 on 2017/11/12.
+ */
+$("#commentPage").addClass("active");
